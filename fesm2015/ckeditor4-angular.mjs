ea71af0c48c5bda73714ac870b002b76ba94a0b3
Spencer Gresoro <spencergresoro@gmail.com>
2025-02-23 17:55:07 -0800

    Update ckeditor4-angular.mjs

--- fesm2015/ckeditor4-angular.mjs
+++ fesm2015/ckeditor4-angular.mjs
@@ -212,6 +212,7 @@ class CKEditorComponent {
         console.log('Set ckeditor id to control id');
         if (elementId) {
             console.log('set..');
+            CKEDITOR.timestamp = new Date().getTime();
             element.id = elementId;
         }
         this.elementRef.nativeElement.appendChild(element);
